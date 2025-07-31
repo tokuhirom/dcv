6d9ca3499585725abcf9131be52cf3ef92b7ab07
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-08-01 00:29:26 +0900

    Add --projects flag to show project list at startup
    - Added command line flag --projects to start with project list view
    - Modified NewModelWithOptions to accept showProjects parameter
    - Updated Init() to load projects when showProjects is true
    - Also added 'p' shortcut key to switch to project list from process list view

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ func main() {
 	// Parse command-line flags
 	var projectName string
 	var composeFile string
+	var showProjects bool
 	flag.StringVar(&projectName, "p", "", "Specify project name")
 	flag.StringVar(&composeFile, "f", "", "Specify compose file")
+	flag.BoolVar(&showProjects, "projects", false, "Show project list at startup")
 	flag.Parse()
 
 	// Create the initial model with options
-	m := ui.NewModelWithOptions(projectName, composeFile)
+	m := ui.NewModelWithOptions(projectName, composeFile, showProjects)
 
 	// Create the program
 	p := tea.NewProgram(m, tea.WithAltScreen())
