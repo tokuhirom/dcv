312c528c9f25db210703c95cd0958a941dcf2222
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-08-08 22:46:29 +0900

    refactor table

---
+++ internal/ui/table.go
@@ -0,0 +1,35 @@
+package ui
+
+import (
+	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func RenderTable(columns []table.Column, rows []table.Row, availableHeight int, currentRow int) string {
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(availableHeight-4),
+	)
+
+	// Set styles
+	tableStyle := table.DefaultStyles()
+	tableStyle.Header = tableStyle.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	tableStyle.Selected = tableStyle.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+
+	t.SetStyles(tableStyle)
+	t.Focus()
+
+	// Move to the selected row
+	t.MoveDown(currentRow)
+
+	return t.View()
+}
